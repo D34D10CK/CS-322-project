1c168c4e0d01ad45f09438435c0cde98bf3760e1
D34D10CK <leonard.berney@epfl.ch>
2017-04-30 19:07:16 +0200

    remove BOM from files

--- data preparation scripts/cleanup.ps1
+++ data preparation scripts/cleanup.ps1
@@ -1,1 +1,7 @@
-Get-ChildItem *csv | foreach {(cat $_ -Encoding utf8) -replace '(?!(?<=\\,)(?=,))(?<=,)(?=,)|(?<=,)$', "\N" -replace ',NULL,', ",\N," | Out-File (join-path -path .\processed\ -ChildPath $_.Name) -Encoding utf8}
+Get-ChildItem *csv | foreach {
+    $csv = (cat $_ -Encoding utf8) -replace '(?!(?<=\\,)(?=,))(?<=,)(?=,)|(?<=,)$', "\N" -replace ',NULL,', ",\N,";
+    # | Out-File (join-path -path .\processed\ -ChildPath $_.Name) -Encoding utf8
+    $path = join-path -path .\processed\ -ChildPath $_.Name | resolve-path;
+    $Utf8NoBomEncoding = New-Object System.Text.UTF8Encoding $False
+    [IO.File]::WriteAllLines($path, $csv, $Utf8NoBomEncoding)
+}
